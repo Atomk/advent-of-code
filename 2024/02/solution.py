a32f62b7cc1f8ff4f9702ee7f06c3cb5a349dc07
Atomk <marco.fras@hotmail.it>
2024-12-03 15:05:49 +0100

    add 2024 day 2
    The second part does not wor, I'll fix it in another commit.
    I did it yesterday in the evening but was too sleepy to finish
    the second part and commit.

---
+++ 2024/02/solution.py
@@ -0,0 +1,83 @@
+def part1(data_file_path):
+    file = open(data_file_path)
+    safe_count = 0
+
+    for report in file:
+        levels = [int(num) for num in report.split()]
+        i = 0
+        is_increasing = levels[i] < levels[i + 1]
+        safe = True
+        while i < len(levels) - 1:
+            if levels[i] == levels[i + 1]:
+                safe = False
+                break
+            elif is_increasing:
+                if not (1 <= levels[i + 1] - levels[i] <= 3):
+                    safe = False
+                    break
+            else:
+                if not (1 <= levels[i] - levels[i + 1] <= 3):
+                    safe = False
+                    break
+            i += 1
+
+        if safe:
+            safe_count += 1
+
+    return safe_count
+
+assert(part1("./sample_input.txt") == 2)
+assert(part1("./input.txt") == 516)
+# print("Answer:", part1("./input.txt"))
+
+# This took me 35 minutes including reading the prompt, gosh I was slower than expected.
+# I think I could have done it in 20 min with less wondering "is this the best way to do it"
+# and more Python knowledge (I had to look a couple things up for splitting)
+# I was tricked by the list initially being strings and not numbers, this is where typed languages clearly win
+# The I forgot to put the "not" in the conditions to break the levels loop, I spent some time thinking what could be the best way to write those checks
+# Let's do part 2
+
+
+def part2(data_file_path):
+    file = open(data_file_path)
+    safe_count = 0
+
+    for report in file:
+        levels = [int(num) for num in report.split()]
+        i = 0
+        is_increasing = levels[i] < levels[i + 1]
+        safe = True
+        already_skipped_one = False
+        while i < len(levels) - 1:
+            if levels[i] == levels[i + 1]:
+                safe = False
+            elif is_increasing:
+                if not (1 <= levels[i + 1] - levels[i] <= 3):
+                    safe = False
+            else:
+                if not (1 <= levels[i] - levels[i + 1] <= 3):
+                    safe = False
+
+            if not safe:
+                if already_skipped_one:
+                    break
+                else:
+                    #if (i + 1) == len(levels) - 1:
+                    levels.pop(i)
+                    already_skipped_one = True
+                    continue
+
+            i += 1
+
+        if safe:
+            safe_count += 1
+
+    return safe_count
+
+# TODO this does not work, I know the reason but I was really tired and fell asleep while thinking up a proper solution
+# I understood the assignment incorrectly and I also lost some time on premature optimization
+# I'll put this on hold so I can work on day 3 but I'm sure I can solve this in the optimal way
+# If it turns out to be unreadable I'll just brute-force it, we can accept compromises
+
+assert(part2("./sample_input.txt") == 4)
+print("Answer:", part2("./input.txt"))
