7cd4098b54cb856dafe5ecf9119a402d1ccfb435
Atomk <marco.fras@hotmail.it>
2024-12-13 15:27:04 +0100

    docs: add note, update README

--- 2024/12/solution_part2.py
+++ 2024/12/solution_part2.py
@@ -156,6 +156,37 @@ def part2(data_file_path) -> int:
 """
 1 hour 30 minutes.
 *sigh*
+
+For a bit I was wondering if I used the right structure to save fences.
+I could also have used a list with single items, it would have been easier
+to reason about, but instead of expensive O(n) removals from list in that case
+I would have marked already checked fences so I could skip them easily.
+Something like this:
+for i in range(len(fences)):
+	if already_checked[i]:
+		continue
+That said, regions are not that big, so even removals from a list
+should not be that big of a deal, but I still think my approach
+with a dictionary of positions has its merits.
+
+As you can see I spent some time playing with ideas and wondering
+how to save fences to make processing them easier and faster.
+
+For directions I really should have used enums instead of strings...
+Especially because on a line I wrote "botttom" with 3 't' and that
+was a bug waiting to happen.
+
+Also in get_sides() I has some technical difficulties and needed
+a couple attempts before finding a control flow that felt good.
+
+When it came to actual compilation, I found two bugs.
+One was a copy-paste error in row_increment that kept the original value,
+and the only logic error was that I forgot to also remove the
+"reference" fence at the end of each iteration of the loop.
+With this in particular I wasted at least 10-15 minutes by wrestling with
+the VSCode debugger, which was giving some "file not found" errors.
+To be fair this was the first time I used the VScode debugger for Python
+(which is actually really useful) and it was a bit unexpected.
 """
 
 answer_sample = part2("./sample_input_1.txt")
