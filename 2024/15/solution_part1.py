d1057a95c9a70e098efd3fac0020921eeb73385e
Atomk <marco.fras@hotmail.it>
2024-12-15 22:26:27 +0100

    docs: add notes to day 15 part 1

--- 2024/15/solution_part1.py
+++ 2024/15/solution_part1.py
@@ -103,6 +103,51 @@ def day15(data_file_path) -> int:
 
     return sum_gps_coords
 
+"""
+Did it in almost exactly one hour.
+Note that despite things being similar to old puzzles, I always wrote
+things from scratch without referencing old solutions (reason: as an exercise)
+
+There were three input files (two samples and the proper input) and
+a few minutes were spent setting up those and the rest of the boilerplate.
+
+Some notes by looking at undo history:
+First thing I wrote the file load and the final coordinates count,
+as they were the easiest things. I refactored things a few times,
+because a bit after coding directions/velocity I came up with a better
+approach and I also felt code would be simpler with a Pos class,
+like the last few puzzles.
+
+The "find direction" (after said little refactoring) and the
+"find next free tile before wall" parts were easy, while surprisingly
+the "move everything in the line" part stumped me for a bit. In an attempt
+to simplify that part I expanded the Pos class by adding methods, used those
+where necessary and used the Pos class more instead of having two separate
+variables for row and col. Not really necessary but it helped simplifying some logic.
+While searching for a warning (apparently you cannot annotate a method parameter
+with the same type of the class it's part of) I found out you can do
+operator overloading in Python, that's handy and dangerous. I tried it but soon
+changed my mind, with Python being dynamically typed it feels a bit scary to have
+this kind of "hidden control flow" that Zig is so adamant about avoiding.
+
+After attempting for a while to understand and implement said movement part,
+it felt harder than necessary and it was clear something was wrong in my approach,
+I just couldn't wrap my head around it. So I draw a picture and then it hit me,
+it's MUCH easier to do in reverse! No temp variables needed or replaced tiles.
+It feels like one of those algorithms/patterns that everyone who studied DSA knows,
+and I'm just not there yet. Yet, but hey, I'm practicing at least.
+
+However, bingo! This was definitely the right approach.
+The good news is, despite me taking a bit long to finish this, all three tests passed first try.
+The only other time I executed the code was after implementing the file load,
+to ensure the map was parsed correctly (left the print statement as a comment)
+
+---
+I'll skip the second part because it looks annoying and long and I want to do
+some other things today. I'd also like to finish the second for the previous days
+where I skipped it (or I made an attempt and failed, or better, stopped trying)
+"""
+
 answer_sample = day15("./sample_input_small.txt")
 print("Answer (sample small):", answer_sample)
 assert(answer_sample == 2028)
